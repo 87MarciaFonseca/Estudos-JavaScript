99d6929a6567282b7b3e31b8c935f974106183b0
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-02-19 19:41:43 -0300

    A distinção de váriaveis

---
+++ comentarios.js
@@ -0,0 +1,1 @@
+console.log(Teste) // Apenas para mostrar como faz comentários
---
+++ variaveisConstantes.js
@@ -0,0 +1,16 @@
+var a = 3 
+let b = 4  // let é possível mudar e evita erros no código do que var
+
+var a = 30
+ b = 40
+
+console.log( a,b)
+
+a = 300
+b = 400 
+
+console.log(a,b)
+
+const c = 5 // const é usado quando há motivo de mudança dentro código
+//c = 50
+console.log(c)
