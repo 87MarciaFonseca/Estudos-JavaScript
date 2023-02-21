416131587dc404ac027185099f36f6f82c633410
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-02-21 14:50:49 -0300

    Novas regras para utilizar number

--- numberAlgunsCuidados.js
+++ numberAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7/0);
 console.log("10" /2)
+console.log('3' + 2)
 console.log("show!" *2)
 console.log(0.1 +0.7) // Gera uma imprecissão
 //console.logt(10.toString()) modo errado
---
+++ string.js
@@ -0,0 +1,14 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3)) // código relacionado a tabela unicode
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0,3)) //selecionar variaveis de uma string
+
+console.log('Escola '.concat(escola).concat("!")) // concatenar variavies
+console.log(escola.replace(3,'e'))
+console.log(escola.replace(/\w/g,'e')) //REgex para subst. de variável
+console.log('Ana,Maria,Pedro'.split(',')) //Away é um agrupador de variaveis
