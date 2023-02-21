485375139a6b8a80358e93176c8a21ddfe6d95c8
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-02-21 14:54:39 -0300

    Tipos de strings e sua utilização em JS

--- string.js
+++ string.js
@@ -9,6 +9,8 @@ console.log(escola.substring(1))
 console.log(escola.substring(0,3)) //selecionar variaveis de uma string
 
 console.log('Escola '.concat(escola).concat("!")) // concatenar variavies
+
+console.log('Escola ' + (escola) +("!")) // outra forma de concatenar
 console.log(escola.replace(3,'e'))
 console.log(escola.replace(/\w/g,'e')) //REgex para subst. de variável
 console.log('Ana,Maria,Pedro'.split(',')) //Away é um agrupador de variaveis
