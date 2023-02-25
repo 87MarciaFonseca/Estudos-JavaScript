c9e3780fa683d43ebaea0bf194d121d649dcfabf
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-02-25 20:34:20 -0300

    Desenvolvendo Objects em JS,um exemplo de jSON

---
+++ object.js
@@ -0,0 +1,30 @@
+const prod1 = {} //object em Js é combição de '{}' e 'valor'
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 49998.90
+prod1['Desconto Legal'] = 0.40// evitar atributos com espaço
+
+// console.log(prod1)
+
+
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    
+}
+console.log(prod2)
+
+//'{"nome":"Camisa Polo","preco: 79.90"}'ex:JSON ,ele é formato textual
+
+//exemplo de conjutos de objects
+//const prod2 = {
+    //nome: 'Camisa Polo',
+   // preco: 79.90,
+   // obj: {
+        //obj: 1,
+       // obj:{
+          //  blabla: 2
+      //  }
+    //}
+//}
+//console.log(prod2)
