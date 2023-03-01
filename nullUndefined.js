7feadd836f99dbd01ac72a174f03563860e73cb9
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-01 10:55:21 -0300

    Utilização do conceito de null e undefined

--- nullUndefined.js
+++ nullUndefined.js
@@ -3,3 +3,11 @@ console.log(valor)
 
 valor = null // ausência de valor
 console.log(valor)
+//console.log(valor.toString())//Erro!
+
+const produto ={}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
