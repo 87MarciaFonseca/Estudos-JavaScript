b08ed1bb5b0ec121307841342de2b66e69d5c0ef
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-06 21:31:42 -0300

    usando VAR suas aplicações

--- funcao2.js
+++ funcao2.js
@@ -5,6 +5,7 @@ const imprimirSoma = function(a,b){
 
 imprimirSoma(2 ,3)
 
+
 //Armazenando uma funcao arrow em uma variavel
 const soma = (a, b) =>{
     return a + b
@@ -13,7 +14,7 @@ const soma = (a, b) =>{
 console.log(soma(2,3))
 
 //retorno implícito
-const subtracao = (a,b) => a- b
+const subtracao = (a,b) =>  a - b
 console.log(subtracao(2,3))
 
 const imprimir2 = a => console.log(a)
