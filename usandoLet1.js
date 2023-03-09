7dcc776002848a6e8e7a4340a49dab96bc6f9058
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-09 18:08:50 -0300

    Explicação da palavra reservada 'let' e 'var'

--- usandoLet1.js
+++ usandoLet1.js
@@ -1,6 +1,6 @@
-var numero = 1
+var numero = 1 // palavra reservada 'var'escopo global e escopo de função
 {
-    let numero = 2
-    console.log('dentro =', numero)
+    let numero = 2 //palavra reservada 'let'em escopo de bloco,global e escopo de função
+    console.log('dentro =', numero) // let está em bloco'{}'
 }
 console.log('fora =', numero)
