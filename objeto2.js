66ac25c0fa74d7fe2a05a9898ef50860b749aace
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-10 16:31:46 -0300

    estacnciamento de objetos

---
+++ objeto2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object) // estanciar um objeto
+
+const Cliente = function(){}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6) ,versão nova do JS
+console.log(typeof Produto)
+console.log(typeof new Object ())
