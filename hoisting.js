3ea91c74ca997aff67f3f8272ca31034043dd275
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-10 14:52:55 -0300

    iguiçamento de var e let ,aplicando hoisting

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+console.log('a =', a) // a variável 'var' será iguiçada,jogada para cima
+var a = 2
+console.log('a =', a) // explicação para mostrar não necessidade de hoisting
+ 
+console.log('b =', b) // o efeito de iguiçamento não funciona em 'let'
+let b = 2
+console.log('b =', b)
+
+
