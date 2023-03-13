92017c7d1c051bf7c9fd2c0c6dad88e68543eba3
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-13 16:59:24 -0300

    notação com utilização de ponto

---
+++ notacaoPonto.js
@@ -0,0 +1,6 @@
+console.log(Math.ceil(6.1))
+
+const objt1 = {}
+objt1.nome = 'Bola' // anotaçõa ponto '.'
+ //objt1['nome'] = 'BoLa2' // opção é a mesma ,muda pode acessar por uma[]
+console.log(objt1.nome)
