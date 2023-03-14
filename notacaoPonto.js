c90181a2ee5b8b568ff73e1e059f24214a36e050
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-14 16:20:19 -0300

    continução da anotação ponto e sua utilização

--- notacaoPonto.js
+++ notacaoPonto.js
@@ -4,3 +4,14 @@ const objt1 = {}
 objt1.nome = 'Bola' // anotaçõa ponto '.'
  //objt1['nome'] = 'BoLa2' // opção é a mesma ,muda pode acessar por uma[]
 console.log(objt1.nome)
+
+function Obj(nome){
+    this.nome = nome
+}
+
+const obj2 = new Obj('Cadeira')
+const obj3 = new Obj('Mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+
+
