edc92e6b9013a7e274fe2639420c7806cfcc41c5
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-22 15:31:16 -0300

    operadores relacioais ,identificar valor e tipo

--- relacionais.js
+++ relacionais.js
@@ -1,5 +1,5 @@
-console.log ('01)', '1' == 1 )
-console.log ('02) ', '1' === 1)
+console.log ('01)', '1' == 1 ) //op. binário ,'==' ,identifica um number
+console.log ('02) ', '1' === 1) // '===' identifica um tipo
 console.log ('03) ',  '3' !=3)
 console.log('04)', '3' !== 3)
 
