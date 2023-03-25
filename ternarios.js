3bd113586e8f7c41beb4aace35c42a890870a97c
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-03-24 21:37:23 -0300

    Ternários é um if  e else resumidos

---
+++ ternarios.js
@@ -0,0 +1,6 @@
+const resultado = nota => nota >= 7 ? 'Aprovado ' : 'Reprovado' //op ternário '>='
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
+
+//const status = nota >= 7 ? 'Aprovado' : 'Reprovado'// ternário armazena e retorna a constante
