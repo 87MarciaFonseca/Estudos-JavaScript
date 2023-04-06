f9cdf0aee557c59b4ddc0bb9e065ccbf76e5c81f
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-06 16:50:44 -0300

    atualização

--- controle/doWhile.js
+++ controle/doWhile.js
@@ -9,6 +9,6 @@ let opcao = -1 // somente iniciada a variável,sem valor incial!
 do {
     opcao = getInteiroAleatorioEntre(-1,10)
     console.log(`Opcão escolhida foi ${opcao}.`)
-} while (opcao != -1) // do while a expressão fica depois do bloco
+} while (opcao != -1) // Do while a expressão fica depois do bloco
 
 console.log('Até a próxima!') // fazendo isso a estrutura vá rodar pelo menos uma vez
