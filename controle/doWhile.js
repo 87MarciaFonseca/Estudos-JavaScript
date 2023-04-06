28d9f30c19011000b8846431d79d6bd2da349cdf
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-06 16:15:23 -0300

    Do while,não é necessário valor inicial com while

---
+++ controle/doWhile.js
@@ -0,0 +1,14 @@
+function getInteiroAleatorioEntre(min,max){
+    const valor = Math.random() * (max -min) + min
+    return Math.floor(valor)
+}
+
+let opcao = -1 // somente iniciada a variável,sem valor incial!
+
+
+do {
+    opcao = getInteiroAleatorioEntre(-1,10)
+    console.log(`Opcão escolhida foi ${opcao}.`)
+} while (opcao != -1) // do while a expressão fica depois do bloco
+
+console.log('Até a próxima!') // fazendo isso a estrutura vá rodar pelo menos uma vez
