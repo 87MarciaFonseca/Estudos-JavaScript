b332c65d881dc0ab81eccf886ac2ddf0aeeb6145
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-08 16:18:31 -0300

    atualização de erro

--- controle/for1.js
+++ controle/for1.js
@@ -10,7 +10,7 @@ for(let i = 1; i <= 10; i++){ // contador com mesma lógica de while em for,só 
 
 const notas = [6.7,7.4,9.8,8.1,7.7] // contador em array,mesma lógica dos dois acima ,só usa uma array para navagar do primeiro até ultimo elemento
 for(let i = 0; i< notas.length; i++){ //notas. 'length' é o tamanho do array ate seu final e 'let' e usado manter o formato do bloco
-     console.log(`nota = ${notas[j]}`)
+     console.log(`nota = ${notas[i]}`)
 }
 
 //console.log(i) // sem usar o let ,dá espaço para usar essa configuração,que passa do laço acima
---
+++ controle/for2.js
@@ -0,0 +1,5 @@
+const notas = [6.7,7.4,9.8,8.1,7.7] // caso ele vai percorrer pelos os inídices e não valores
+
+for(i of notas){
+    console.log(i,notas[i])
+}
