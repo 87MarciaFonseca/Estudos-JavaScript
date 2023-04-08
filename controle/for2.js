9d082ffab1c0236537b9c083dc25e09637854bab
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-08 16:34:49 -0300

    For in e suas aplicaçoes em objetos

--- controle/for2.js
+++ controle/for2.js
@@ -1,5 +1,16 @@
 const notas = [6.7,7.4,9.8,8.1,7.7] // caso ele vai percorrer pelos os inídices e não valores
 
-for(i of notas){
-    console.log(i,notas[i])
+for(let i in notas){ // interessante usar 'lete' para amarrar os blocos no laço
+    console.log(i,notas[i]) //ele acessa primeiro o índice e depois o array
+}
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 29,
+    peso: 64
+}
+
+for(let atributo in pessoa){ // atrinuto de um objeto
+    console.log(`${atributo} = ${pessoa[atributo]}`)// percorrer atrib. em objtos
 }
