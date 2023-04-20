cc74dc2d25af1db971cba54b8ce27b41eeae5431
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-20 16:50:05 -0300

    outra maneira de percorrer um array ,pelo browser

---
+++ funcao/callback3.js
@@ -0,0 +1,7 @@
+//Exemplo de callback no browser
+document.getElementsByTagName('body')[0].onclick= function(e){
+    console.log('O evento ocorreu!')//vai percorrer o array,por clicks
+}
+
+
+//esse exemplo foi executado no browser
