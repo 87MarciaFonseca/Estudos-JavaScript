e68b69b19de06ffc8d236ec30cb31413350ebc9d
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-21 18:29:13 -0300

    tipos de delcaraçoes, formas de utilisação

--- funcao/tiposDeclaracoes.js
+++ funcao/tiposDeclaracoes.js
@@ -1,14 +1,18 @@
-//function declaration
+console.log(soma(3,4))
+//function declaration- diferença,que essa função pode ser chamada antes ex:
 function soma(x,y){
     return x + y
-}
+}// são usadas bastantes em códigos
 
-// function expression
+// function expression- só pode ser chamada depois da declaração
 const sub = function (x,y){
     return x - y
-}
+}// são usadas bastantes em códigos
+console.log(sub(3,4))
 
-// named function expression
+// named function expression-só pode ser chamada depois da declaração
 const mult = function mult(x,y){
     return x * y
 }
+
+console.log(mult(3,4))
