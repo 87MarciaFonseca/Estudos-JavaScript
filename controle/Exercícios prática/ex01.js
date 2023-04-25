2a67bb1a4cecd69f65c9e4ab8077bfd0d9011b96
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-25 16:38:40 -0300

    Calcular operadores,exercícios de fixação 01.

---
+++ controle/Exercícios prática/ex01.js
@@ -0,0 +1,9 @@
+//crie uma função que dado dois valores(passados como parêmetros)
+//mostra no console a soma, subtração ,multic.e divisao desses valores
+
+function calcularOperadores(operador1,operador2){
+    console.log(operador1 + operador2,operador1 * operador2,operador1 / operador2)
+}
+
+calcularOperadores(1,2)
+    
