850b89b5e8591cd2926cdd09268d5ae5bd6ee821
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-04-26 16:55:10 -0300

    Classsificar triângulos exercicíos

---
+++ controle/Exercícios prática/ex02.js
@@ -0,0 +1,15 @@
+function classificaTrinagulo(lado1 , lado2 ,lado3){
+    if(lado2 == lado2 && lado2 == lado3){
+         return " Equilátero" 
+    } else if(lado1 == lado2 || lado2 == lado3 || lado1 == lado3){
+         return  "Isósceles" 
+    
+    }else{
+        return "Escaleno"
+    }
+    
+}
+
+console.log(classificaTriangulo(2, 2, 2));
+console.log(classificaTriangulo(2, 3, 3));
+console.log(classificaTriangulo(2, 3, 4));
