f8bb0649bf3609d819210a2c4b3a4d2ef2e3542b
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-05-09 23:02:21 -0300

    herença parte1 aplicações e anotações

--- Objeto/heranca1.js
+++ Objeto/heranca1.js
@@ -1,4 +1,4 @@
-const ferrari = {
+const ferrari = { // objeto
     modelo: ' F40',
     velMax: 324
 }
@@ -9,10 +9,10 @@ const volvo = {
 
 }
 
-console.log(ferrari.__proto__)
-console.log(ferrari.__proto__ === Object.prototype)
+console.log(ferrari.__proto__) // usando '__proto__' eu acesso uma cadeia de protótipo.pai
+console.log(ferrari.__proto__ === Object.prototype) // apontam para mesmo object.prototype,acesso um atib.do objeto
 console.log(volvo.__proto__ === Object.prototype)
-console.log(Object.prototype.__proto__ === null)
+console.log(Object.prototype.__proto__ === null) //aponta p/ object.prototype é null
 
 
 function MeuObjeto(){}
