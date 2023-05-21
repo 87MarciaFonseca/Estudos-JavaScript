6d74bba4d9e28d8b27b27a29b6009ddcda363d75
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-05-21 20:22:33 -0300

    Array,visão geral

---
+++ controle/Array/array.js
@@ -0,0 +1,25 @@
+// Visão Geral
+console.log(typeof Array , typeof new Array, typeof[])
+
+let aprovados = new Array('Bia','Carlos','Ana')
+console.log(aprovados)
+
+
+aprovados = ['Bia', 'Carlos','Ana']
+console.log (aprovados[0])
+console.log (aprovados[1])
+console.log (aprovados[2])
+console.log (aprovados[3])
+
+aprovados[3] = 'Paulo'
+aprovados.push('Abia')
+console.log(aprovados.length)
+
+
+aprovados[9] = ' Rafael'
+console.log(aprovados.length)
+console.log(aprovados[8]) === undefined
+
+console.log(aprovados)
+console.log(aprovados.sort())
+
