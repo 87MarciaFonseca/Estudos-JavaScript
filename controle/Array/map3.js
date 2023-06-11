e7d02ef284f63c769d577bc37a3b1095dd123e35
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-06-11 14:48:56 -0300

    Filter com array,exemplos

--- controle/Array/map3.js
+++ controle/Array/map3.js
@@ -18,5 +18,8 @@ const carrinho = [
 const paraObjeto = json => JSON.parse(json)
 const apenasPreco = produto => produto.preco
 
-const resultado = carrinho.map(paraObjeto).map(apenasPreco)
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
 console.log(resultado)
+
+
+
