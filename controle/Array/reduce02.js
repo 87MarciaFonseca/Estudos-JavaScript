0a59f0ec3bdc9bd23ddb146aeef163552fc38c25
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-07-04 21:15:07 -0300

    Continuação com explicação

--- controle/Array/reduce02.js
+++ controle/Array/reduce02.js
@@ -2,7 +2,7 @@ const alunos = [
     {nome: 'João',nota: 7.3,bolsista: false},
     {nome: 'Maria',nota: 9.2,bolsista: true},
     {nome: 'Pedro',nota: 9.8,bolsista: false},
-    {nome: 'Ana',nota: 8.7,bolsista: false},
+    {nome: 'Ana',nota: 8.7,bolsista: true},
 ]
 
 
@@ -14,3 +14,5 @@ console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
 
 // Desafio 2: algum aluno é bolsista?
 
+const algumBolsista =(resultado,bolsista) => resultdo || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
