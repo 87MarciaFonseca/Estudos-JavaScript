ccbe6b35ba8a3d95d5f57af854e50487a36d5566
87MarciaFonseca <marcia.fonseca.ti@gmail.com>
2023-07-05 16:47:25 -0300

    Exemplos de imperativo e declarativo

---
+++ controle/Array/imperativoVSDeclarativo.js
@@ -0,0 +1,20 @@
+const alunos =[
+    {nome:'João',nota: 7.9},
+    {nome:'Maria',nota: 9.2}
+]
+
+
+//Imperativo
+
+let total1 = 0
+for (var i = 0; i < alunos.length; i++){
+    total1 += alunos[i].nota
+}
+console.log(total1 / alunos.length)
+
+// Declarativo
+const getNota = aluno => aluno.nota
+const soma = (total,atual)=> total + atual
+
+const total2 = alunos.map(getNota).reduce(soma) // Reduce de função de agregar
+console.log(total2 / alunos.length)
